7aedc0100f51b1cc4d9a5bd9b8d1f72204d49956
Abhijeet Ghawade <abhijeet.ghawade12@gmail.com>
2021-02-11 20:12:54 +0530

    Add files via upload

---
+++ segmentcentroid/tfmodel/MiceModel.py
@@ -0,0 +1,31 @@
+from .TFSeparableModel import TFSeparableModel
+from .supervised_networks import *
+
+class GridWorldModel(TFSeparableModel):
+    
+    """
+    This class defines the abstract class for a tensorflow model for the primitives.
+    """
+    #The action dimension for this will have to change?  Hidden layers can stay the same 
+    def __init__(self, 
+                 k,
+                 statedim=(12,2), 
+                 actiondim=(24,1), 
+                 hidden_layer=8):
+
+        self.hidden_layer = hidden_layer
+        
+        super(GridWorldModel, self).__init__(statedim, actiondim, k, [0,1], 'cluster')
+
+
+    def createPolicyNetwork(self):
+
+        #return multiLayerPerceptron(self.statedim[0], self.actiondim[0])
+        return gridWorldTabular(12, 2,24 )
+
+    def createTransitionNetwork(self):
+
+        return gridWorldTabular( 12,2, 2)
+
+        
+
